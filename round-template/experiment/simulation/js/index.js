3cabc8f12a015c4df11b315dd596a73557a5451e
Subhashree Baskaran <69038330+Subhashree2172@users.noreply.github.com>
2021-07-07 20:17:37 +0530

    Update index.js

--- round-template/experiment/simulation/js/index.js
+++ round-template/experiment/simulation/js/index.js
@@ -5,7 +5,7 @@ canvas.height = 500;
 var c = canvas.getContext('2d');
 // ----------------------------------------------------------------------
 //Initiating the global values
-var frequency = Number(5)// var time_period = Number(5);
+var frequency = Number(5) // var time_period = Number(5);
 var y_pos = Number(0);
 var amp = Number(30);
 var x_pos = Number(0);
@@ -33,7 +33,8 @@ var q2_count = 0;
 var q3_count = 0;
 var q4_count = 0;
 var q5_count = 0;
-var tour_check  = 0;
+var tour_check = 0;
+quest_answered = 1;
 // plotAxes();-----------------------------------------------------
 function plotAxes() {
   c.clearRect(0, 0, canvas.width, canvas.height);
@@ -110,81 +111,94 @@ function plot_condn() {
 // CRO  PARAMETERS----------------------------------------
 //Time period-------------------------------------------------------------------
 function timeperiod() {
-  if (on == 1 && on_fg == 1) {
-    time_period = Number(document.getElementById("time_division").value);
-    x_div = time_period;
-    plot_condn();
-  }
-  if (tour_on == 8) { //For demo purpose
-    dialog.close();
-    dialog = document.getElementById('yposition_cro_demo');
-    dialog.show();
-    tour_on = 9;
-  }
+    if (on == 1 && on_fg == 1) {
+      time_period = Number(document.getElementById("time_division").value);
+      x_div = time_period;
+      plot_condn();
+    }
+    if (tour_on == 8) { //For demo purpose
+      dialog.close();
+      dialog = document.getElementById('yposition_cro_demo');
+      dialog.show();
+      tour_on = 9;
+    }
+
+
 }
 // // Position of Y changing----------------------------------------------------
 function yposition() {
-  if (on == 1 && on_fg == 1) {
-    y_pos = Number(document.getElementById("yposition").value);
-    plot_condn();
-  }
-  if (tour_on == 9) { //For demo purpose
-    dialog.close();
-    dialog = document.getElementById('ydivision_cro_demo');
-    dialog.show();
-    tour_on = 10;
-  }
+
+    if (on == 1 && on_fg == 1) {
+      y_pos = Number(document.getElementById("yposition").value);
+      plot_condn();
+    }
+    if (tour_on == 9) { //For demo purpose
+      dialog.close();
+      dialog = document.getElementById('ydivision_cro_demo');
+      dialog.show();
+      tour_on = 10;
+    }
+
+
 }
 // //Position of X-Axis---------------------------------------------------------
 function xposition() {
-  if (on == 1 && on_fg == 1) {
-    x_pos = Number(document.getElementById("xposition").value);
-    plot_condn();
-  }
-  if (tour_on == 7 ){  //For demo purpose
-    dialog.close();
-    dialog = document.getElementById('xdivision_cro_demo');
-    dialog.show();
-    tour_on = 8;
-  }
+
+    if (on == 1 && on_fg == 1) {
+      x_pos = Number(document.getElementById("xposition").value);
+      plot_condn();
+    }
+    if (tour_on == 7) { //For demo purpose
+      dialog.close();
+      dialog = document.getElementById('xdivision_cro_demo');
+      dialog.show();
+      tour_on = 8;
+    }
+
+
 }
 //Changing the amplitude-----------------------------------------------------
 function ydivision() {
-  if (on == 1 && on_fg == 1) {
-    y_div = 1 / Number(document.getElementById("ydivision_input").value);
-    plot_condn();
-  }
-  if (tour_on == 10){ //For demo purpose
-    dialog.close();
-    dialog = document.getElementById('xy_cro_demo');
-    dialog.show();
-    tour_on = 11;
-  }
+
+    if (on == 1 && on_fg == 1) {
+      y_div = 1 / Number(document.getElementById("ydivision_input").value);
+      plot_condn();
+    }
+    if (tour_on == 10) { //For demo purpose
+      dialog.close();
+      dialog = document.getElementById('xy_cro_demo');
+      dialog.show();
+      tour_on = 11;
+    }
+
+
 }
 //ON/OFF switch--------------------------------------------------
 function on_off_cro() {
-  if (tour_on == 2) { //For demo purpose
-    dialog.close();
-    dialog = document.getElementById('onoff_fg_demo');
-    dialog.show();
-    tour_on = 3
-  }
-  count = count + 1;
-  if (count % 2 == 0) {
-    on = 1;
-    document.getElementById("canvas").style.backgroundColor = "rgba(0,255,0,0.6)"
-    if (on_fg == 1) {
-      window.requestAnimationFrame(plotfunc);
-    } else {
-      window.requestAnimationFrame(plotAxes);
+
+    if (tour_on == 2) { //For demo purpose
+      dialog.close();
+      dialog = document.getElementById('onoff_fg_demo');
+      dialog.show();
+      tour_on = 3
     }
-    result();
-  } else {
-    on = 0;
-    document.getElementById("canvas").style.backgroundColor = "rgba(0,255,0,0.1)"
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    result();
-  }
+    count = count + 1;
+    if (count % 2 == 0) {
+      on = 1;
+      document.getElementById("canvas").style.backgroundColor = "rgba(0,255,0,0.6)"
+      if (on_fg == 1) {
+        window.requestAnimationFrame(plotfunc);
+      } else {
+        window.requestAnimationFrame(plotAxes);
+      }
+      result();
+    } else {
+      on = 0;
+      document.getElementById("canvas").style.backgroundColor = "rgba(0,255,0,0.1)"
+      c.clearRect(0, 0, canvas.width, canvas.height);
+      result();
+    }
+
 
 }
 //X-Y-----------------------------------------------------------------------------
@@ -202,36 +216,41 @@ function plotLine() {
 }
 
 function xy() {
-  if (tour_on == 11) {
-    dialog.close();
-    dialog = document.getElementById('focus_cro_demo');
-    dialog.show();
-    tour_on = 12
-  }
-  xycount = xycount + 1;
-  if (xycount % 2 == 0) {
-    xy_on = 1;
-    plot_condn();
-    result();
-  } else {
-    xy_on = 0;
-    plot_condn();
-    result();
-  }
+
+    if (tour_on == 11) {
+      dialog.close();
+      dialog = document.getElementById('focus_cro_demo');
+      dialog.show();
+      tour_on = 12
+    }
+    xycount = xycount + 1;
+    if (xycount % 2 == 0) {
+      xy_on = 1;
+      plot_condn();
+      result();
+    } else {
+      xy_on = 0;
+      plot_condn();
+      result();
+    }
+
+
 }
 //Focus -----------------------------------------------------
 function plot_focus() {
-  plotAxes();
-  c.strokeStyle = "blue";
-  c.beginPath();
-  var y = (canvas.height / 2) - 5;
-  c.moveTo(x_pos, y);
-  for (i = x_pos; i < 2000; i++) {
-    y = ((canvas.height / 2) - (y_pos * 1));
-    c.lineTo(i, y);
-  }
-  c.stroke();
-  result();
+
+    plotAxes();
+    c.strokeStyle = "blue";
+    c.beginPath();
+    var y = (canvas.height / 2) - 5;
+    c.moveTo(x_pos, y);
+    for (i = x_pos; i < 2000; i++) {
+      y = ((canvas.height / 2) - (y_pos * 1));
+      c.lineTo(i, y);
+    }
+    c.stroke();
+    result();
+
 }
 
 function myfunc() {
@@ -263,30 +282,62 @@ function connect_wire() {
     dialog.show();
     tour_on = 2
   }
-  q1_count = q1_count +1;
-  if( q1_count == 1 && tour_check == 0){
+  q1_count = q1_count + 1;
+  if (q1_count == 1 && tour_check == 0) {
     dialog_out = document.getElementById('pop_up_quest_1');
     dialog_out.show();
     quest_no = 1;
+    quest_answered = 0;
+    disable();
   }
 }
 
 function remove_wire() {
-  document.getElementById("wire").style.top = "225px";
-  connect = 0;
-  plot_condn();
-  if (tour_on == 13) {
-    tour_on = 0;
-    dialog.close();
-    tour_check = 0;
+
+    document.getElementById("wire").style.top = "225px";
+    connect = 0;
+    plot_condn();
+    if (tour_on == 13) {
+      tour_on = 0;
+      dialog.close();
+      tour_check = 0;
+    }
+    result();
+    q5_count = q5_count + 1;
+    if (q5_count == 1 && tour_check == 0) {
+      dialog_out = document.getElementById('pop_up_quest_5');
+      dialog_out.show();
+      quest_no = 5;
+      quest_answered = 0;
+      disable();
+    }
+
+
+}
+// Function disabler
+function disable() {
+  var x = " "
+  if(quest_answered ===0 ){
+    x = "true";
   }
-  result();
-  q5_count = q5_count +1;
-  if( q5_count == 1 && tour_check == 0){
-    dialog_out = document.getElementById('pop_up_quest_5');
-    dialog_out.show();
-    quest_no = 5;
+  else{
+    x=false
   }
+  document.getElementById("time_division").disabled = x;
+  document.getElementById("how_to_measure").disabled = x;
+  document.getElementById("on_off_cro").disabled = x;
+  document.getElementById("yposition").disabled = x;
+  document.getElementById("xposition").disabled = x;
+  document.getElementById("xy").disabled = x;
+  document.getElementById("focus").disabled = x;
+  document.getElementById("y_division").disabled = x;
+  document.getElementById("connect").disabled = x;
+  document.getElementById("remove").disabled = x;
+  document.getElementById("on_off_fg").disabled = x;
+  document.getElementById("amplitude").disabled = x;
+  document.getElementById("frequency").disabled = x;
+  document.getElementById("signal_select").disabled = x;
+  document.getElementById("tour").disabled = x;
 }
 //FUNCTION GENERATOR----------------------------------------------------------------------\
 var display = document.getElementById("display_canvas");
@@ -304,182 +355,205 @@ function display_fg() {
 }
 
 function amplitude() {
-  if (on_fg == 1) {
-    amp = Number(document.getElementById("amplitude").value);
-    amp_disp = amp;
-    display_fg();
-  }
-  if (on == 1) {
-    plot_condn();
-    result();
-  }
-  if (tour_on == 6 ){ //For demo purpose
-    dialog.close();
-    dialog = document.getElementById('xposition_cro_demo');
-    dialog.show();
-    tour_on = 7;
-  }
-  q4_count = q4_count +1;
-  if( q4_count == 1 && tour_check == 0){
-    dialog_out = document.getElementById('pop_up_quest_4');
-    dialog_out.show();
-    quest_no = 4;
-  }
+
+    if (on_fg == 1) {
+      amp = Number(document.getElementById("amplitude").value);
+      amp_disp = amp;
+      display_fg();
+    }
+    if (on == 1) {
+      plot_condn();
+      result();
+    }
+    if (tour_on == 6) { //For demo purpose
+      dialog.close();
+      dialog = document.getElementById('xposition_cro_demo');
+      dialog.show();
+      tour_on = 7;
+    }
+    q4_count = q4_count + 1;
+    if (q4_count == 1 && tour_check == 0) {
+      dialog_out = document.getElementById('pop_up_quest_4');
+      dialog_out.show();
+      quest_no = 4;
+      quest_answered = 0;
+      disable();
+    }
+
+
 }
 
 function freq() {
-  if (on_fg == 1) {
-    freq_disp = Number(document.getElementById("frequency").value);
-    frequency = freq_disp;
-    display_fg();
-  }
-  if (on == 1) {
-    plot_condn();
-    result();
-  }
-  if (tour_on == 5 ){  //For demo purpose
-    dialog.close();
-    dialog = document.getElementById('amplitude_fg_demo');
-    dialog.show();
-    tour_on = 6;
-  }
-  q3_count = q3_count +1;
-  if( q3_count == 1 && tour_check == 0){
-    dialog_out = document.getElementById('pop_up_quest_3');
-    dialog_out.show();
-    quest_no = 3;
-  }
+
+    if (on_fg == 1) {
+      freq_disp = Number(document.getElementById("frequency").value);
+      frequency = freq_disp;
+      display_fg();
+    }
+    if (on == 1) {
+      plot_condn();
+      result();
+    }
+    if (tour_on == 5) { //For demo purpose
+      dialog.close();
+      dialog = document.getElementById('amplitude_fg_demo');
+      dialog.show();
+      tour_on = 6;
+    }
+    q3_count = q3_count + 1;
+    if (q3_count == 1 && tour_check == 0) {
+      dialog_out = document.getElementById('pop_up_quest_3');
+      dialog_out.show();
+      quest_no = 3;
+      quest_answered = 0;
+      disable();
+    }
+
+
 }
 
 function on_off_fg() {
-  if (count_fg % 2 == 0) {
-    document.getElementById("display_canvas").style.backgroundColor = "rgba(0,255,0,0.6)"
-    display_fg();
-    on_fg = 1;
-    plot_condn();
-  } else {
-    on_fg = 0;
-    disp.clearRect(0, 0, canvas.width, canvas.height);
-    document.getElementById("display_canvas").style.backgroundColor = "rgba(0,255,0,0.1)"
-    result();
-    plot_condn();
-  }
-  count_fg = count_fg + 1;
-  if (tour_on == 3) { //For demo purpose
-    dialog.close();
-    dialog = document.getElementById('signal_select_demo');
-    dialog.show();
-    tour_on = 4
-  }
+
+    if (count_fg % 2 == 0) {
+      document.getElementById("display_canvas").style.backgroundColor = "rgba(0,255,0,0.6)"
+      display_fg();
+      on_fg = 1;
+      plot_condn();
+    } else {
+      on_fg = 0;
+      disp.clearRect(0, 0, canvas.width, canvas.height);
+      document.getElementById("display_canvas").style.backgroundColor = "rgba(0,255,0,0.1)"
+      result();
+      plot_condn();
+    }
+    count_fg = count_fg + 1;
+    if (tour_on == 3) { //For demo purpose
+      dialog.close();
+      dialog = document.getElementById('signal_select_demo');
+      dialog.show();
+      tour_on = 4
+    }
+
+
 }
 
 function signal_selection() {
-  if (on_fg == 1) {
-    signal_no = document.getElementById("signal_select").value;
-    if (signal_no == 1) {
-      func = "Sin";
-    } else if (signal_no == 2) {
-      func = "Cos";
-    } else if (signal_no == 3) {
-      func = "Sawtooth";
-    } else if (signal_no == 4) {
-      func = "Triangle";
-    } else if (signal_no == 5) {
-      func = "Square"
+    if (on_fg == 1) {
+      signal_no = document.getElementById("signal_select").value;
+      if (signal_no == 1) {
+        func = "Sin";
+      } else if (signal_no == 2) {
+        func = "Cos";
+      } else if (signal_no == 3) {
+        func = "Sawtooth";
+      } else if (signal_no == 4) {
+        func = "Triangle";
+      } else if (signal_no == 5) {
+        func = "Square"
+      }
+      display_fg();
+      result();
     }
-    display_fg();
-    result();
-  }
-  if (on == 1) {
-    window.requestAnimationFrame(plotfunc);
-  }
-  if (tour_on == 4 ){
-    dialog.close();
-    dialog = document.getElementById('frequency_fg_demo');
-    dialog.show();
-    tour_on = 5;
-  }
-  q2_count = q2_count +1;
-  if( q2_count == 1 && tour_check == 0){
-    dialog_out = document.getElementById('pop_up_quest_2');
-    dialog_out.show();
-    quest_no = 2;
-  }
+    if (on == 1) {
+      window.requestAnimationFrame(plotfunc);
+    }
+    if (tour_on == 4) {
+      dialog.close();
+      dialog = document.getElementById('frequency_fg_demo');
+      dialog.show();
+      tour_on = 5;
+    }
+    q2_count = q2_count + 1;
+    if (q2_count == 1 && tour_check == 0) {
+      dialog_out = document.getElementById('pop_up_quest_2');
+      dialog_out.show();
+      quest_no = 2;
+      quest_answered = 0;
+      disable();
+    }
+
+
 }
 // Tour guide
 function tour() {
-  tour_on = 1;
-  tour_check = 1;
-  dialog = document.getElementById('connect_demo');
-  dialog.show();
+
+    tour_on = 1;
+    tour_check = 1;
+    dialog = document.getElementById('connect_demo');
+    dialog.show();
+
+
 }
 // Result
 function result() {
-  if(func == "Sin" || func == "Cos"){
-    peak_value = amp * 0.707;
-    avg_value = amp* 0.637;
-  }
-  else if (func=="Square") {
-    peak_value = amp;
-    avg_value = amp;
-  }
-  else{
-    peak_value = amp*0.577;
-    avg_value = amp*0.5;
-  }
-  if (on == 1 && on_fg == 1 && connect == 1) {
-    document.getElementById("timeperiod_result").textContent = ":" + (1 / frequency) + "s"
-    document.getElementById("ptop_voltage_result").textContent = ":" + amp * 2 + "V";
-    document.getElementById("rms_voltage_result").textContent = ":" + peak_value + "V";
-    document.getElementById("average_volatge_result").textContent = ":" + avg_value + "V";
-    document.getElementById('peak_voltage_result').textContent = ":" + amp + "V";
-  }
-  else{
-    document.getElementById("timeperiod_result").textContent = ":" ;
-    document.getElementById("ptop_voltage_result").textContent = ":";
-    document.getElementById("rms_voltage_result").textContent = ":";
-    document.getElementById("average_volatge_result").textContent = ":" ;
-    document.getElementById('peak_voltage_result').textContent = ":" ;
-  }
+
+    if (func == "Sin" || func == "Cos") {
+      peak_value = amp * 0.707;
+      avg_value = amp * 0.637;
+    } else if (func == "Square") {
+      peak_value = amp;
+      avg_value = amp;
+    } else {
+      peak_value = amp * 0.577;
+      avg_value = amp * 0.5;
+    }
+    if (on == 1 && on_fg == 1 && connect == 1) {
+      document.getElementById("timeperiod_result").textContent = ":" + (1 / frequency) + "s"
+      document.getElementById("ptop_voltage_result").textContent = ":" + amp * 2 + "V";
+      document.getElementById("rms_voltage_result").textContent = ":" + peak_value + "V";
+      document.getElementById("average_volatge_result").textContent = ":" + avg_value + "V";
+      document.getElementById('peak_voltage_result').textContent = ":" + amp + "V";
+    } else {
+      document.getElementById("timeperiod_result").textContent = ":";
+      document.getElementById("ptop_voltage_result").textContent = ":";
+      document.getElementById("rms_voltage_result").textContent = ":";
+      document.getElementById("average_volatge_result").textContent = ":";
+      document.getElementById('peak_voltage_result').textContent = ":";
+    }
+
+
 }
 
 // POPup question function
-for(i=0;i<document.querySelectorAll(".answer").length;i++){
-  document.querySelectorAll(".answer")[i].addEventListener("click", function(){
+for (i = 0; i < document.querySelectorAll(".answer").length; i++) {
+  document.querySelectorAll(".answer")[i].addEventListener("click", function() {
     var value = this.value;
-    ans_display(Number(value),quest_no);
-  } ); }
-function ans_display(ans,quest_no){
-  if(ans == 1){
+    ans_display(Number(value), quest_no);
+  });
+}
+
+function ans_display(ans, quest_no) {
+  if (ans == 1) {
     alert("Correct answer!");
-  }
-  else{
-    switch(quest_no){
+  } else {
+    switch (quest_no) {
       case 1:
-      alert("Wrong answer!"+" \nCorrect answer: Peak Voltage/√2");
-      break;
+        alert("Wrong answer!" + " \nCorrect answer: Peak Voltage/√2");
+        break;
 
       case 2:
-      alert("Wrong answer!"+"\nCorrect answer: 21.21V \nPeak Voltage = Peak-to-peak/2 = 30V \nRMS voltage = Peak Voltage/√2 =30/√2 = 21.21 V");
-      break;
+        alert("Wrong answer!" + "\nCorrect answer: 21.21V \nPeak Voltage = Peak-to-peak/2 = 30V \nRMS voltage = Peak Voltage/√2 =30/√2 = 21.21 V");
+        break;
 
       case 3:
-      alert("Wrong answer!"+"\nCorrect answer: 0.1s \n Timeperiod = 1/Frequency \n T = 1/10 = 0.1s");
-      break;
+        alert("Wrong answer!" + "\nCorrect answer: 0.1s \n Timeperiod = 1/Frequency \n T = 1/10 = 0.1s");
+        break;
 
       case 4:
-      alert("Wrong answer!"+"\n Correct answer: Average Value");
-      break;
+        alert("Wrong answer!" + "\n Correct answer: Average Value");
+        break;
 
       case 5:
-      alert("Wrong answer!"+"\nCorrect answer: 11.54V\nRMS value of sawtooth wave=Peak value/√3 = 20/√3 =11.54V");
-      quest_no = 0;
-      break;
+        alert("Wrong answer!" + "\nCorrect answer: 11.54V\nRMS value of sawtooth wave=Peak value/√3 = 20/√3 =11.54V");
+        quest_no = 0;
+        break;
     }
   }
- dialog_out.close();
- quest_answered = 1;
+  dialog_out.close();
+  quest_answered = 1;
+  disable();
 }
+
 function measurement_guide() {
   alert("Each division in CRO is 10 units.After taking the reading from the display multiply with the X-Division,Y-Division if X,Y related parameter is calculated.Press the Tour button to get familiarize with the functionality of CRO and function generator");
 }
